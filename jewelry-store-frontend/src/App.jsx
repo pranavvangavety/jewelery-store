722ced7cce1d1b369017c3ab36c1545fc007a3d7
pranavvangavety <vangavetypranav@gmail.com>
2026-03-10 06:55:37 -0400

    frontend: configure application routing and root setup

--- jewelry-store-frontend/src/App.jsx
+++ jewelry-store-frontend/src/App.jsx
@@ -3,16 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
 
 function App() {
 
   return (
     <BrowserRouter>
+        <NavBar/>
         <Routes>
             <Route path="/" element={<div>Home Page</div>} />
-            <Route path="/login" element={<div>Home Page</div>}/>
-            <Route path="/register" element={<div>Register Page</div>}/>
-            <Route path="/products" element={<div>Products Page</div>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/products" element={<ProductsPage/>}/>
             <Route path="/cart" element={<div>Cart Page</div>}/>
         </Routes>
     </BrowserRouter>
