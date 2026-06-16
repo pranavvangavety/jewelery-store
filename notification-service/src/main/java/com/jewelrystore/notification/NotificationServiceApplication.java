b5802eb9803e94093643efdc775395212ae9b04e
pranavvangavety <vangavetypranav@gmail.com>
2026-06-16 15:59:34 -0400

    notification-service: deserialize Kafka events via message converter

--- notification-service/src/main/java/com/jewelrystore/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/jewelrystore/notification/NotificationServiceApplication.java
@@ -3,13 +3,25 @@ package com.jewelrystore.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.converter.ByteArrayJsonMessageConverter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NotificationServiceApplication.class, args);
+
+
     }
+
+    @Bean
+    public ByteArrayJsonMessageConverter jsonMessageConverter() {
+        return new ByteArrayJsonMessageConverter();
+    }
+
+
 
 }
